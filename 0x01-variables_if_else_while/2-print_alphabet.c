cd21f9855301c74ac8f2388921afac06560b13e3
Lwazman <lwazijiyane962@gmail.com>
2023-02-17 03:16:49 -0800

    Lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
  * main - Entry point
  *
@@ -8,11 +6,11 @@
  */
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (char letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
-		printf("\n");
+		putchar('\n');
+	}
 
 	return (0);
 }
