c9346fa0b20dab4232e2c9821d65b44eb9dd0ee3
Lwazman <lwazijiyane962@gmail.com>
2023-02-17 02:54:58 -0800

    Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+		printf("\n");
+
+	return (0);
+}
