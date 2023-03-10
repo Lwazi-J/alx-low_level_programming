94c892d5a6933f7b3207f6f6931673f4e1d73ed5
Lwazman <lwazijiyane962@gmail.com>
2023-03-10 07:44:58 -0800

    Libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -10,6 +10,6 @@ void _puts(char *str)
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	_puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
