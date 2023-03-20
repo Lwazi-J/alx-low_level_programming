1464ddf1bf1507baaab2c28a98991fce2d71edbd
Lwazman <lwazijiyane962@gmail.com>
2023-03-20 11:26:12 -0700

    new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,16 +6,6 @@
  */
 typedef struct dog dog_t;
 
-/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-
-struct dog
 {
 	char *name;
 	float age;
