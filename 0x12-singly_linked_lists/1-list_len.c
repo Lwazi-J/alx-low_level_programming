0e36484b051b2d457c91f6719f5da699741cc53b
Lwazi Jiyane <67145874+Lwazman@users.noreply.github.com>
2023-04-28 19:47:27 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,6 +7,7 @@
  *
  * Return: number of elements in h
  */
+
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
@@ -16,6 +17,7 @@ size_t list_len(const list_t *h)
 		n++;
 		h = h->next;
 	}
+	
 	return (n);
 }
 
