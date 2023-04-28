12eecffa22467c3240836c7dc69e182e139598c3
Lwazi Jiyane <67145874+Lwazman@users.noreply.github.com>
2023-04-28 19:48:26 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,6 +9,7 @@
  *
  * Return: address of the new element, or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
@@ -36,7 +37,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp = temp->next;
 
 	temp->next = new;
-
 	return (new);
 }
 
