ac9f016d1c4d759d4a0a09599933deb37b270c65
Lwazi-J <lwazijiyane962@gmail.com>
2023-07-06 06:45:11 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,24 @@
  * Return: the converted number
  */
 
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+    unsigned int result = 0;
+    int i;
 
-	if (!b)
-		return (0);
+    if (b == NULL)
+        return 0;
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
+    for (i = 0; b[i] != '\0'; i++) {
+        if (b[i] != '0' && b[i] != '1')
+            return 0;
 
-	return (dec_val);
+        result = result * 2 + (b[i] - '0');
+    }
+
+    return resut;
 }
+
+
 
